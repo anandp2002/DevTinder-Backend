5f1e5703cdb91bfa72513cabe4a687161bd55cc6
Anand P <anandpamegoze@gmail.com>
2024-12-27 17:02:45 +0530

    C:/Program Files/Git/feed api

--- src/app.js
+++ src/app.js
@@ -9,22 +9,33 @@ app.use(express.json());
 
 app.post('/signup', async (req, res) => {
   const user = new User(req.body);
-
   try {
     await user.save();
-    res.status(201).send('User added successfully');
+    res.status(201).send('User added successfully !');
   } catch (err) {
-    res.status(400).send('Error creating user ', err.message);
+    res.status(400).send('Error creating user : ', err.message);
+  }
+});
+
+app.get('/feed', async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) {
+      res.send('No users available !');
+    }
+    res.status(200).send(users);
+  } catch {
+    res.send('Error in /feed api ');
   }
 });
 
 connectDB()
   .then(() => {
-    console.log('Database connected successfully');
+    console.log('Database connected successfully !');
     app.listen(3000, () => {
-      console.log('Server is listening on port 3000');
+      console.log('Server is listening on port 3000...');
     });
   })
   .catch(() => {
-    console.log('Error while connecting to DB');
+    console.log('Error while connecting to DB !');
   });
