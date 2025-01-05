889b211f1c4d5da790a7a67d734e37829cee4df7
Anand P <anandpamegoze@gmail.com>
2025-01-05 16:36:46 +0530

    user/connections api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,15 @@
 import ConnectionRequest from '../models/connectionRequest.js';
 
+const USER_SAFE_DATA = [
+  'firstName',
+  'lastName',
+  'photoUrl',
+  'age',
+  'gender',
+  'about',
+  'skills',
+];
+
 // Get all the pending connection requests for the logged in user
 export const requests = async (req, res) => {
   try {
@@ -7,19 +17,28 @@ export const requests = async (req, res) => {
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: 'interested',
-    }).populate('fromUserId', [
-      'firstName',
-      'lastName',
-      'photoUrl',
-      'age',
-      'gender',
-      'about',
-      'skills',
-    ]);
+    }).populate('fromUserId', USER_SAFE_DATA);
     return res.status(200).json({
       message: 'Data fetched successfully !',
       data: connectionRequests,
     });
+  } catch (err) {
+    return res.status(400).send('ERROR : ' + err.message);
+  }
+};
+
+export const connections = async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connections = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: 'accepted' },
+        { toUserId: loggedInUser._id, status: 'accepted' },
+      ],
+    }).populate('fromUserId', USER_SAFE_DATA);
+    const data = connections.map((row) => row.fromUserId);
+
+    return res.status(200).json({ data: data });
   } catch (err) {
     return res.status(400).send('ERROR : ' + err.message);
   }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { requests } from '../controllers/user.controller.js';
+import { requests, connections } from '../controllers/user.controller.js';
 import { protectRoute } from '../middlewares/auth.middleware.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/requests/received', protectRoute, requests);
+userRouter.get('/connections', protectRoute, connections);
 
 export default userRouter;
