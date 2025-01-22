1b99c17bad7d98c5a4ba3d0ac2c68f9d1d380b92
Anand P <anandpamegoze@gmail.com>
2025-01-22 10:37:16 +0530

    update methods in cors

--- src/server.js
+++ src/server.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(
   cors({
     origin: ['http://localhost:5173', 'http://192.168.1.3:5173'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   })
 );
